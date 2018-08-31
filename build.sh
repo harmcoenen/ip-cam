7b9ff374d3637e873f7da55bcbdf116ab8853ed6
Harm Coenen <harm@familiecoenen.nl>
2018-08-31 21:47:09 +0200

    Adding travis build script for automated builds after commits.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp /mnt/hgfs/SharedFolder/ip-cam/* /home/harm/github/ip-cam/.
+cp /mnt/hgfs/SharedFolder/github/ip-cam/* /home/harm/github/ip-cam/.
 
 gcc ipCamGstCapt.c -o ipCamGstCapt `pkg-config --cflags --libs gstreamer-video-1.0 gtk+-3.0 gstreamer-1.0`
