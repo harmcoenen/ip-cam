9ef6abb879630c66788e474d71920d3f3dcb2b40
Harm Coenen <harm@familiecoenen.nl>
2018-08-31 23:12:12 +0200

    Have travis install gstreamer libs.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp '/mnt/hgfs/SharedFolder/github/ip-cam/.travis.yml' /home/harm/github/ip-cam/.
 cp /mnt/hgfs/SharedFolder/github/ip-cam/* /home/harm/github/ip-cam/.
 
-gcc ipCamGstCapt.c -o ipCamGstCapt `pkg-config --cflags --libs gstreamer-video-1.0 gtk+-3.0 gstreamer-1.0`
+gcc ipCamGstCapt.c -o ipCamGstCapt `pkg-config --cflags --libs gstreamer-video-1.0 gstreamer-1.0`
